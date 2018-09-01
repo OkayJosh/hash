64b1334ddae990b2ec58e2a8a9a7d2d09a871158
Olatunji Joshua <19627177+OkayJosh@users.noreply.github.com>
2018-09-01 12:58:42 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Joshua</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+         <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+            .box {
+
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="container">
+                <div class="content">
+                    <div class="title">Olatunji joshua</div>
+                </div>
+                <div class="content"> <h4>My name is joshua kayode, 
+                i strongly believe in the development and advancement of 
+                the collective intelligence of the human race, the sum total 
+                of all intelligent mind that have been here on the planet up till now,
+                 their contribution and impact. if we could further that, 
+                 improve on their strive, we will only cause intelligent advancement
+                  to our own species, will will further our biology, our planet will
+                   be peaceful, and we will eradicate suffering and all it vices.
+                My area of interest spans from Artificial intelligence,
+                philosophy to spirituality of the modern age. 
+                This area of interest has a strong pull on me because i see them as
+                 providing a powerful tool that empowers the mind, seeing the future
+                  from an optimistic position, and allowing us to take control of our 
+                  own destiny as a specie. </h4> </div>
+                <div class="links flex-center">
+                        <a href="/blog">blog</a>
+                        <a href="/projects">Projects</a>
+                        <a href="/future">Futurist</a>
+                        <a href="https://www.linkedin.com/in/joshua-olatunji-95524688/">LinkedIn</a>
+                        <a href="https://github.com/okayjosh">GitHub</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
