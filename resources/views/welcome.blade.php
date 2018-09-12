76e3a17288b4ebc5cf76a770645039b65d5d2e1b
Olatunji Joshua <19627177+OkayJosh@users.noreply.github.com>
2018-09-12 01:15:13 +0100

    changes to welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,138 +1,41 @@
-@extends('layouts.parent')
+@extends ('layouts.parent')
 
-@section('title', 'Joshua')
+@section ('title','Home')
 
-@section('navigation')
+<!-- inheriting layout -->
+@section('navigation') 
     @parent
 @endsection
 
+
+<!-- Main Content -->
 @section('content')
-<style>
-            html, body {
-                background-color: #fff;
-                color: #007bff;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .boxshadow {
-                width: 100%;
-                padding: 10px;
-                position: relative;
-                box-shadow: 2px 5px 10px ;
-            }
-        </style>
-<div class="flex-center position-ref full-height">
-            <div class="container">
-                <div class="content">
-                    <div class="title">Olatunji joshua</div>
-                </div>
-                <div class="content boxshadow"> <h4> 
-                I strongly believe in the development and advancement of 
-                the collective intelligence of the human race, the sum total 
-                of all intelligent mind that have been here on the planet up till now,
-                 their contribution and impact. if we could further that, 
-                 improve on their strive, we will only cause intelligent advancement
-                  on our own specie, will will further our biology, our planet will
-                   be peaceful, and we will eradicate suffering and all it vices.
-                My area of interest spans from Artificial intelligence,
-                philosophy to spirituality of the modern age. 
-                This area of interest has a strong pull on me because i see them as
-                 providing a powerful tool that empowers the mind, seeing the future
-                  from an optimistic position, and allowing us to take control of our 
-                  own destiny as a specie. </h4> </div>
-            </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-preview">
+            <a href="#">
+              <h2 class="post-title">
+                
+              </h2>
+              <h3 class="post-subtitle">
+                
+              </h3>
+            </a>
+            <p class="post-meta">Posted by
+              <a href="#">Start Bootstrap</a>
+              on September 24, 2018</p>
+          </div>
+          <hr>
+          
+          <!-- Pager -->
+          <div class="clearfix">
+            <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+          </div>
         </div>
+      </div>
+    </div>
+    <hr>
 @endsection
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Joshua</title>
-        
-
-    </head>
-    <body>
-    <div class="container">
-    <div class="links justify-content-end">
-                        <ul class="nav justify-content-end">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/blog">Blog</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.linkedin.com/in/joshua-olatunji-95524688/">LinkedIn</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://github.com/okayjosh">GitHub</a>
-                        </li>
-                        </ul>
-                    </div> 
-
-    </div>
-    </body>
-</html>
--->
