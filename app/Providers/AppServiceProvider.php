d6a69303b9ce6f254870d84b045d4bd0feba1abd
Olatunji Joshua <19627177+OkayJosh@users.noreply.github.com>
2018-09-12 01:08:57 +0100

    including application wide  BlogModel access

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Blog;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +16,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        $lastBlog = Blog::latest();
+        view()->share('lastBlog', '$lastBlog');
     }
 
     /**
